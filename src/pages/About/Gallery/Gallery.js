5bf2cfb459fe46c1dda4319e8a3c1ed902511e4f
Nathan Sparks <nate.p.sparks@gmail.com>
2023-09-09 20:20:38 -0500

    Third Commit

--- src/pages/About/Gallery/Gallery.js
+++ src/pages/About/Gallery/Gallery.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Gallery.css'
+import React, { useState } from 'react';
+import './Gallery.css';
 
 import imageItem1 from "../../../assets/gallery01.jpg";
 import imageItem2 from "../../../assets/gallery02.jpg";
@@ -13,49 +13,24 @@ import imageItem9 from "../../../assets/Quesadilla.jpg";
 import imageItem10 from "../../../assets/Tenders.jpg";
 
 const Gallery = () => {
+  const imageUrls = [imageItem1, imageItem2, imageItem3, imageItem4, imageItem5, imageItem6, imageItem7, imageItem8, imageItem9, imageItem10];
+
+  const [selectedImageIndex, setSelectedImageIndex] = useState(-1);
+
   return (
-    <div className='gallery'>
-        <figure className='gallery__item gallery__item-1'>
-            <img src={imageItem1} alt="Cat" className='gallery__img' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-2'>
-            <img src={imageItem2} alt="Dog" className='gallery__img2' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-3'>
-            <img src={imageItem3} alt="Burger" className='gallery__img3' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-4'>
-            <img src={imageItem4} alt="Sandwich" className='gallery__img4' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-5'>
-            <img src={imageItem5} alt="Steak" className='gallery__img5' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-6'>
-            <img src={imageItem6} alt="Onion Rings" className='gallery__img6' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-7'>
-            <img src={imageItem7} alt="Pizza" className='gallery__img7' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-8'>
-            <img src={imageItem8} alt="Pasta" className='gallery__img8' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-9'>
-            <img src={imageItem9} alt="Quesadilla" className='gallery__img9' />
-        </figure>
-
-        <figure className='gallery__item gallery__item-10'>
-            <img src={imageItem10} alt="Chicken Tenders" className='gallery__img10' />
-        </figure>
+    <div className="gallery-container">
+      {imageUrls.map((imageUrl, index) => (
+        <div key={index} onClick={() => setSelectedImageIndex(index)}>
+          <img src={imageUrl} alt={`Gallery Image ${index + 1}`} className="gallery-image" />
+        </div>
+      ))}
+      {selectedImageIndex !== -1 && (
+        <div>
+          <img src={imageUrls[selectedImageIndex]} alt={`Selected Image ${selectedImageIndex + 1}`} className="selected-image" />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
